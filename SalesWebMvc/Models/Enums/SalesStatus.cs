af599775904b17d4050c1e0318f3394749fed8c7
Bernardo Tomaz <bernardo.tomaz@hotmail.com>
2024-08-12 10:48:36 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/Enums/SalesStatus.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace SalesWebMvc.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0,
+        Billed = 1,
+        Canled = 2 
+    }
+}
---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,29 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public double Amount { get; set; }
+        public DateTime Date { get; set; }
+        public int Id { get; set; }
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+
+        public SalesRecord(double amount, DateTime date, int id, SaleStatus status, Seller seller)
+        {
+            Amount = amount;
+            Date = date;
+            Id = id;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
