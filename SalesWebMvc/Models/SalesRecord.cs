bc1b3219e3b09acbc068c8c0cff39652820464bf
Bernardo Tomaz <bernardo.tomaz@hotmail.com>
2024-09-24 13:50:37 -0300

    Implementing grouping search

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,13 +1,14 @@
 ﻿using SalesWebMvc.Models.Enums;
 using System;
-
-
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
     public class SalesRecord
     {
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Amount { get; set; }
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy}")]
         public DateTime Date { get; set; }
         public int Id { get; set; }
         public SaleStatus Status { get; set; }
